3fa902e802612520034e12cd5311ca3c86837ef7
Anna Gorzanowska <anna.gorzanowska@gmail.com>
2021-01-23 14:36:29 +0100

    read word

--- src/main/java/pl/edu/agh/hangman/ReadWords.java
+++ src/main/java/pl/edu/agh/hangman/ReadWords.java
@@ -4,13 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadWords {
 	public static void main(String[] args) throws Exception {
 		readFile("src/main/resources/words.txt");
 	}
 
-	private static void readFile(String filename) throws Exception {
+	private static List<String> readFile(String filename) throws Exception {
+		List<String> wordsList = new ArrayList<String>();
+		
 		File wordsFile = new File(filename);
 		BufferedReader br = null;
 		try {
@@ -18,10 +22,12 @@ public class ReadWords {
 			String contentLine = br.readLine();
 			while (contentLine != null) {
 				System.out.println(contentLine);
+				wordsList.add(contentLine);
 				contentLine = br.readLine();
 			}
 		} catch (IOException x) {
 			System.err.format("IOException: %s", x); 
 		}
+		return wordsList;
 	}
 }
