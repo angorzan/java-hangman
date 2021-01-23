3354b8d3bd5436efa4422297b757c0afa3acf6be
Konrad Bukowicz <bukowicz@student.agh.edu.pl>
2021-01-23 16:06:14 +0100

    Chnages in DrawImage - added HANGMANPIC as local

--- src/main/java/pl/edu/agh/hangman/DrawImage.java
+++ src/main/java/pl/edu/agh/hangman/DrawImage.java
@@ -2,6 +2,59 @@ package pl.edu.agh.hangman;
 
 public class DrawImage {
 	
+	 private static final String[] HANGMANPICS = new String[]{
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "=========",
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "  O   |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "=========",
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "  O   |\n" +
+	                    "  |   |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "=========",
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "  O   |\n" +
+	                    " /|   |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "=========",
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "  O   |\n" +
+	                    " /|\\  |\n" +
+	                    "      |\n" +
+	                    "      |\n" +
+	                    "=========",
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "  O   |\n" +
+	                    " /|\\  |\n" +
+	                    " /    |\n" +
+	                    "      |\n" +
+	                    "=========",
+	            "  +---+\n" +
+	                    "  |   |\n" +
+	                    "  O   |\n" +
+	                    " /|\\  |\n" +
+	                    " / \\  |\n" +
+	                    "      |\n" +
+	                    "========"
+	    };
+
+	
 	public int getNumberOfBadTries() {
 		return numberOfBadTries;
 	}
@@ -12,30 +65,31 @@ public class DrawImage {
 
 	private static int numberOfBadTries = 0;
 	
-	public void drawImage() {
+	
+	public static void drawImage(String guessWord) {
 		
-		int numberOfHangmanImage = Hangman.HANGMANPICS.length;
+//		int numberOfHangmanImage = Hangman.HANGMANPICS.length;
 	
 		if(numberOfBadTries == 1) {
-			System.out.print(Hangman.HANGMANPICS[0]);
+			System.out.print(HANGMANPICS[0]);
 		} 
-			else if(numberOfBadTries == 2 && numberOfBadTries < numberOfHangmanImage) {
-			System.out.print(Hangman.HANGMANPICS[1]);
+			else if(numberOfBadTries == 2) {
+			System.out.print(HANGMANPICS[1]);
 		} 
-			else if(numberOfBadTries == 3 && numberOfBadTries < numberOfHangmanImage) {
-			System.out.print(Hangman.HANGMANPICS[2]);
+			else if(numberOfBadTries == 3) {
+			System.out.print(HANGMANPICS[2]);
 		} 
-			else if(numberOfBadTries == 4 && numberOfBadTries < numberOfHangmanImage) {
-			System.out.print(Hangman.HANGMANPICS[3]);
+			else if(numberOfBadTries == 4) {
+			System.out.print(HANGMANPICS[3]);
 		} 
-			else if(numberOfBadTries == 5 && numberOfBadTries < numberOfHangmanImage) {
-			System.out.print(Hangman.HANGMANPICS[4]);
+			else if(numberOfBadTries == 5) {
+			System.out.print(HANGMANPICS[4]);
 		} 
-			else if(numberOfBadTries == 6 && numberOfBadTries < numberOfHangmanImage) {
-			System.out.print(Hangman.HANGMANPICS[5]);
+			else if(numberOfBadTries == 6) {
+			System.out.print(HANGMANPICS[5]);
 		} 
-			else if(numberOfBadTries == 7 && numberOfBadTries < numberOfHangmanImage) {
-			System.out.print(Hangman.HANGMANPICS[6]);
+			else if(numberOfBadTries == 7) {
+			System.out.print(HANGMANPICS[6]);
 		}
 		
 		
