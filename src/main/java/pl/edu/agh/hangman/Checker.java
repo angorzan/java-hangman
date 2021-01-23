2f62316aa3c0d28c930afbb6be0499b63281d84e
Maciek <mblok@student.agh.edu.pl>
2021-01-23 15:19:41 +0100

    kolejny krok

--- src/main/java/pl/edu/agh/hangman/Checker.java
+++ src/main/java/pl/edu/agh/hangman/Checker.java
@@ -2,14 +2,20 @@ package pl.edu.agh.hangman;
 
 public class Checker {
 
-	public void checkChar(char c, String word) {
+	public boolean checkChar(char c, String word) {
+
+		boolean status = false;
+
 		for (char ch : word.toCharArray()) {
 			if (c == ch) {
 				System.out.print(c);
+				status = true;
 			} else {
 				System.out.print("_");
 			}
 			System.out.println();
+
 		}
+		return status;
 	}
 }
