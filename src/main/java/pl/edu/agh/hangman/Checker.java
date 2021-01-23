83584836998b432143bd0ed1bbd9583b909e5f38
Maciek <mblok@student.agh.edu.pl>
2021-01-23 14:47:19 +0100

    Checker update

--- src/main/java/pl/edu/agh/hangman/Checker.java
+++ src/main/java/pl/edu/agh/hangman/Checker.java
@@ -1,5 +1,15 @@
 package pl.edu.agh.hangman;
 
 public class Checker {
-	
+
+	public void checkChar(char c, String word) {
+		for (char ch : word.toCharArray()) {
+			if (c == ch) {
+				System.out.print(c);
+			} else {
+				System.out.print("_");
+			}
+			System.out.println();
+		}
+	}
 }
--- src/main/java/pl/edu/agh/hangman/HangmanDemo.java
+++ src/main/java/pl/edu/agh/hangman/HangmanDemo.java
@@ -3,7 +3,8 @@ package pl.edu.agh.hangman;
 public class HangmanDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
 
 	}
 
